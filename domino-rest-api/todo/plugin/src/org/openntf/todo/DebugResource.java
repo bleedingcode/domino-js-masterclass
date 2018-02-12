0e793ba697c5df19100d6ce8dc6d2ab3c39b4dd4
Paul Withers <pwithers@intec.co.uk>
2018-02-12 18:56:24 +0000

    Validation on DebugResource Completing method for createToDoNSF - creates views now

--- domino-rest-api/todo/plugin/src/org/openntf/todo/DebugResource.java
+++ domino-rest-api/todo/plugin/src/org/openntf/todo/DebugResource.java
@@ -1,7 +1,9 @@
 package org.openntf.todo;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,7 +18,10 @@ public class DebugResource {
 
 	@Path("/toggleProfiling")
 	@GET
-	public Response toggleProfiling() {
+	public Response toggleProfiling(@Context HttpServletRequest request) {
+		if (!validateKey(request)) {
+			return Response.status(401).build();
+		}
 		JsonJavaObject jjo = new JsonJavaObject();
 		jjo.put("profiling", Utils.toggleProfiling());
 		return Response.ok(jjo.toString(), MediaType.APPLICATION_JSON).build();
@@ -24,11 +29,19 @@ public class DebugResource {
 
 	@Path("/personalStoreName")
 	@GET
-	public Response getPersonalStoreName() {
+	public Response getPersonalStoreName(@Context HttpServletRequest request) {
+		if (!validateKey(request)) {
+			return Response.status(401).build();
+		}
 		JsonJavaObject jjo = new JsonJavaObject();
 		jjo.put("username", Factory.getSession(SessionType.CURRENT).getEffectiveUserName());
 		jjo.put("storename", Utils.getPersonalStoreName(Factory.getSession(SessionType.CURRENT)));
 		return Response.ok(jjo.toString(), MediaType.APPLICATION_JSON).build();
+	}
+
+	private boolean validateKey(HttpServletRequest request) {
+		String verificationKey = request.getHeader("X-TODO-USER-KEY");
+		return DEBUG_KEY.equals(verificationKey);
 	}
 
 }
